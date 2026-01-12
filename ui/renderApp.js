e861d52045d94e797f1b05a027658de5c87a9e64
Egor Pokrovskiy <pokrovskiyegor@internet.ru>
2026-01-13 00:55:33 +0300

    Переписал стили, добавил фильтры.

--- ui/renderApp.js
+++ ui/renderApp.js
@@ -2,7 +2,7 @@ import state from "../state/state.js";
 
 const renderApp = () => {
   const tasksList = document.querySelector('.todo__list')
-  const tasksCount = document.querySelector('.todo__total-tasks > span')
+  const tasksCount = document.querySelector('[data-js-tasks-count]')
 
   tasksList.innerHTML = '' // Очищаем DOM перед рендером
   tasksCount.textContent = state.todos.length // Устанавливаем счетчик по длине массива задач
@@ -21,7 +21,7 @@ const renderApp = () => {
       <p class="todo__item-title">${todo.title}</p>
       ${todo.description !== null ? `<p class="todo__item-description">${todo.description}</p>` : ''} 
       <div class="todo__item-controls">
-        <button class="todo__button delete-button" data-js-delete-task-button>х</button>
+        <button class="todo__button button delete-button" data-js-delete-task-button>Del</button>
       </div>
     </div>`
     tasksList.appendChild(toDoItem)
